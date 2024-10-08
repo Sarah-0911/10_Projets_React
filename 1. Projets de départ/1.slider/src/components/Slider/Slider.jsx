b5c09dbe7c47d51176b770cd4590d87dfed4e6f1
Sarah-0911 <sarah.tabet69@gmail.com>
2024-10-08 15:03:52 +0200

    ex finished alone

--- 1. Projets de départ/1.slider/src/components/Slider/Slider.jsx
+++ 1. Projets de départ/1.slider/src/components/Slider/Slider.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import sliderData from '../../data/sliderData.js'
 import './Slider.css'
 import leftArrow from '../../assets/left-arrow.svg'
@@ -8,18 +8,39 @@ export default function Slider() {
 
   const [showSlide, setShowSlide] = useState(sliderData[0]);
 
+  const leftSlide = () => {
+    const prevSlide = sliderData[showSlide.id - 2];
+
+    if(showSlide.id === 1) {
+      setShowSlide(sliderData[sliderData.length - 1]);
+    } else {
+      setShowSlide(prevSlide);
+    }
+  }
+
+  const rightSlide = () => {
+    const nextSlide = sliderData[showSlide.id];
+
+    if(showSlide.id < sliderData.length) {
+      setShowSlide(nextSlide);
+    }
+    else if (showSlide.id === sliderData.length) {
+      setShowSlide(sliderData[0]);
+    }
+  }
+
   return (
     <div className='container'>
       <p className='sliderCount'>{showSlide.id} / 5</p>
       <div className='slider'>
-        <button className='arrow'>
+        <button onClick={leftSlide} className='arrow'>
           <img src={leftArrow} alt="left arrow" />
         </button>
         <div className="picture">
           <img src={`../../../public/images/img-${showSlide.id}.jpg`} alt={showSlide.description} />
           <span className='description'>{showSlide.description}</span>
         </div>
-        <button className='arrow'>
+        <button onClick={rightSlide} className='arrow'>
           <img src={rightArrow} alt="right arrow" />
         </button>
       </div>

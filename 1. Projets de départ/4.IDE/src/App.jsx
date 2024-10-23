c5b45c81143fab0b5b70c3ac1d2677d1836ed979
Sarah-0911 <sarah.tabet69@gmail.com>
2024-10-23 13:52:36 +0200

    ex.4 Mise en place de base ok

--- 1. Projets de départ/4.IDE/src/App.jsx
+++ 1. Projets de départ/4.IDE/src/App.jsx
@@ -1,6 +1,25 @@
 function App() {
   return (
-    <div>
+    <div className="bg-zinc-900 min-h-screen flex flex-col">
+      <div className="px-4 py-4 border-b flex justify-between items-center">
+        <h1 className="text-2xl w-full text-slate-50">
+          <span className="text-base">the</span>SmartIDE
+        </h1>
+        <div className="w-full">
+          <button className="">Hide Preview</button>
+        </div>
+        <div className="w-full flex">
+          <div className="ml-auto mr-2">
+            <div className="w-[6px] h-[6px] rounded-full bg-slate-200 my-1"></div>
+            <div className="w-[6px] h-[6px] rounded-full bg-slate-200 my-1"></div>
+            <div className="w-[6px] h-[6px] rounded-full bg-slate-200"></div>
+          </div>
+        </div>
+      </div>
+      <div>
+        {/* <Sidebar /> */}
+        {/* <Content /> */}
+      </div>
     </div>
   )
 }

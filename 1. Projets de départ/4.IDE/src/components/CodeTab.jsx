19347e79866bc78d396d03f98fb28c1d7e5a8f51
Sarah-0911 <sarah.tabet69@gmail.com>
2024-10-24 16:33:36 +0200

    ex.4 Ajout de la fonctionnalité de changement de code

--- 1. Projets de départ/4.IDE/src/components/CodeTab.jsx
+++ 1. Projets de départ/4.IDE/src/components/CodeTab.jsx
@@ -1,12 +1,25 @@
-import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { updateCode } from "../features/tabsSlice.js";
 
 export default function CodeTab({ id, code }) {
+
+  const dispatch = useDispatch()
+
+  const handleTextarea = (e) => {
+    dispatch(updateCode({
+      id,
+      value: e.target.value
+    }))
+  }
+
+  
 
   return (
     <textarea
     spellCheck="false"
     className="bg-zinc-900 text-slate-200 p-8 block h-full w-full focus:outline-none resize-none"
     value={code}
+    onChange={handleTextarea}
     >
     </textarea>
   )

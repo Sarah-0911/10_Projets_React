3aa5217b49ba75a36690b3084683068fbc3c69de
Sarah-0911 <sarah.tabet69@gmail.com>
2024-10-30 12:38:43 +0100

    ex.5 fonction thunk in progress

--- 1. Projets de départ/5.chrono/src/components/ToggleButton.jsx
+++ 1. Projets de départ/5.chrono/src/components/ToggleButton.jsx
@@ -1,19 +1,34 @@
-import { useState } from "react"
 import reset from "../assets/reset.svg"
 import play from "../assets/play-button.svg"
+import { useDispatch, useSelector } from "react-redux"
+import { startChrono, resetChrono, setIntervalID } from "../features/chronoSlice.js"
 
 export default function ToggleButton() {
 
+  const chrono = useSelector(state => state.chrono);
+
+  const dispatch = useDispatch();
+
+  const toggleTick = () => {
+    // if (chrono.isPlaying) return;
+
+    if (chrono.intervalID === undefined) {
+      dispatch(startChrono())
+    } else {
+      dispatch(resetChrono())
+    }
+  }
+
   return (
     <button
+    onClick={toggleTick}
     className="flex justify-center items-center gap-3 mx-auto p-2 w-28 border
     bg-slate-300 hover:bg-slate-200 text-slate-900 rounded">
-      <span>Start</span>
+      <span>{chrono.isPlaying ? "Reset" : "Start"}</span>
       <img
       className="w-4"
-      src={play}
+      src={chrono.isPlaying ? reset : play}
       alt="" />
     </button>
-
   )
 }
--- 1. Projets de départ/5.chrono/src/features/chronoSlice.js
+++ 1. Projets de départ/5.chrono/src/features/chronoSlice.js
@@ -43,8 +43,34 @@ export const chronoSlice = createSlice({
         chosenState.value += action.payload.value;
       }
     },
+    tick: (state) => {
+      state.displayedValue.value --;
+      state.isPlaying = true;
+    },
+    resetChrono: (state) => {
+      state.displayedValue.value = 1500;
+      if (state.intervalID) clearInterval(state.intervalID);
+      state.intervalID = undefined;
+      state.isPlaying = false;
+    },
+    setIntervalID: (state, action) => {
+      state.intervalID = action.payload;
+    }
   }
 })
 
-export const { updateChronoValues } = chronoSlice.actions;
+export const startChrono = () => (dispatch, getState) => {
+  console.log(getState());
+
+  if (getState().chrono.intervalID) return;
+
+  dispatch(tick())
+  const intervalID = setInterval(() => {
+    dispatch(tick())
+  }, 1000)
+
+  dispatch(setIntervalID(intervalID))
+}
+
+export const { updateChronoValues, tick, resetChrono, setIntervalID } = chronoSlice.actions;
 export default chronoSlice.reducer;

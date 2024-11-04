d0a94f1510a7028736d4c7af191cfa0401151bab
Sarah-0911 <sarah.tabet69@gmail.com>
2024-11-04 16:05:50 +0100

    ex.6 Gérer l'angle global du dégradé ok

--- 1. Projets de départ/6.gradient-maker/src/components/inputs/AngleRange.jsx
+++ 1. Projets de départ/6.gradient-maker/src/components/inputs/AngleRange.jsx
@@ -1,5 +1,19 @@
+import { useSelector, useDispatch } from "react-redux"
+import { updateAngle } from "../../features/gradientSlice";
+
 export default function AngleRange() {
+
+  const dispatch = useDispatch();
+  const angle = useSelector(state => state.gradient.angle)
+
   return (
-    <input className="w-full" type="range" id="" />
+    <input
+    className="w-full h-1 rounded-lg cursor-pointer"
+    type="range"
+    value={angle}
+    min="0"
+    max="360"
+    onChange={(e) => dispatch(updateAngle(e.target.value))}
+    />
   )
 }

ba3ee8f4c45e8b80571a29a2ab251366d20b6c8d
Sarah-0911 <sarah.tabet69@gmail.com>
2024-11-13 11:36:54 +0100

    ex.7 Créer un panneau de fonctionnalités audio ok

--- 1. Projets de départ/7.songs/src/App.jsx
+++ 1. Projets de départ/7.songs/src/App.jsx
@@ -7,19 +7,13 @@ function App() {
   return (
   <>
     <div className="bg-slate-800 pt-20 min-h-screen">
-
       <div className="max-w-xl mx-auto">
         <Player />
         <h1 className="text-slate-100 text-xl mb-4">PlayerMania - Your songs.</h1>
-
         {/* Playlist */}
         <Playlist />
       </div>
-
-      <div>
         <PlayerPanel />
-      </div>
-
     </div>
   </>
   )
--- 1. Projets de départ/7.songs/src/components/Player/PlayerPanel.jsx
+++ 1. Projets de départ/7.songs/src/components/Player/PlayerPanel.jsx
@@ -1,19 +1,25 @@
+import { useSelector } from "react-redux"
 import ChangeTrackBtn from "./ChangeTrackBtn"
 import PlayPauseBtn from "./PlayPauseBtn"
 import ProgressRange from "./ProgressRange"
-import TrackInfos from "./TrackInfos"
-
-// import { useSelector } from "react-redux"
 
 export default function PlayerPanel() {
 
-  // const playlist = useSelector(state => state.playlist)
-  // console.log(playlist);
+  const playlist = useSelector(state => state.playlist)
+  const currentTrack = playlist.songs?.find(song => song.id === playlist.currentTrackID);
 
   return (
-    <div className="fixed bottom-0 w-full bg-slate-300">
-      <div className="max-w-[600px] text-slate-800 mx-auto p-4">
-      <TrackInfos />
+    <div className="fixed bottom-0 w-full rounded bg-slate-300 border-t-2 border-gray-800">
+      <div className="max-w-[800px] text-slate-800 mx-auto p-4">
+
+      <div className="flex justify-between items-end">
+        <div>
+          <p className="font-semibold">{playlist.songs && currentTrack.title}</p>
+          <p className="text-sm">{playlist.songs && currentTrack.artist}</p>
+        </div>
+        <p className="text-sm">{playlist.songs?.findIndex(song => song.id === playlist.currentTrackID) + 1} / {playlist.songs?.length}</p>
+      </div>
+
       <div className="flex justify-center items-center gap-3">
         <ChangeTrackBtn />
         <PlayPauseBtn />
